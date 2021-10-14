c15b8321d51ccb353635ae49876a6839c277cf98
Steven Stallion <sstallion@gmail.com>
2021-10-14 14:30:36 -0500

    Prepare sources for git

--- bin/makeball.sh
+++ bin/makeball.sh
@@ -1,2 +1,2 @@
 cd /opt/pidp11
-tar cvzf /home/pi/Downloads/pidp11.tar.gz ./bin/ ./install/ ./etc/ ./src/ ./systems/selections ./systems/idled/
+tar cvzf /home/pi/Downloads/pidp11.tar.gz --exclude-vcs ./bin/ ./install/ ./etc/ ./src/ ./systems/selections ./systems/idled/
